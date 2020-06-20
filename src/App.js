b6f7a6970ffc62e1b71e00c2fcc3498ba8478f8e
usama learner <syedusamalearner@gmail.com>
2020-06-20 07:51:46 +0500

    second commnt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Dinner from './dinner.js';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {/* <Dinner></Dinner> */}
+     <Dinner dishName="Nihari" sweetDish="ice cream"></Dinner>
+      <hr />
+      <Dinner dishName='Biryani' sweetDish='Jaleebi' />
+      <hr />
+      <Dinner dishName='Karahi' sweetDish='Gajar ka Halwa' />
+      <Dinner myage={9}></Dinner>
     </div>
   );
 }
-
 export default App;
---
+++ src/dinner.js
@@ -0,0 +1,12 @@
+ 
+import React from 'react';
+function Dinner(props) {
+  return (
+    <div>
+      <h1>Today we are serving {props.dishName}</h1>
+      <h1>Today we are also serving sweet dish {props.sweetDish}</h1>
+  <h1>my age is {props.myage}</h1>
+    </div>
+  );
+}
+export default Dinner;
